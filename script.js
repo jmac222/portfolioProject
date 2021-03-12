93070194b9edfb95056a32393486490d02c2e0e6
John McFarland <john.mcfarland222@gmail.com>
2021-03-12 14:29:40 -0700

    script

--- script.js
+++ script.js
@@ -28,3 +28,48 @@ function autoType(elementClass, typingSpeed){
 $(function(){
   autoType(".text-js", 300)
 })
+
+$(function(){
+
+  //settings for slider
+  let width = 720;
+  let animationSpeed = 2000;
+  let pause = 2500; //200 + 500 delay
+  //cache DOM Elements
+  let $slideContainer = $(`.slides`);
+  let $slides = $(`.slide`);
+
+  let interval;
+  let currentSlide = 1;
+
+  let r1 = Math.floor(Math.random * 255);
+  let r2 = Math.floor(Math.random * 255);
+  let r3 = Math.floor(Math.random * 255);
+  
+
+  //create fuction that starts slider
+  function startSlider(){
+      interval = setInterval(function(){
+          //animating the slide container will cause the action in the function to hapen every delay
+          $slideContainer.animate({'margin-left': `-=${width}`},
+          animationSpeed,
+          function(){
+              currentSlide++;
+              if(currentSlide == $slides.length){
+                  currentSlide = 1;
+                  $slideContainer.css('margin-left', 0);
+              }
+          })
+      }, pause);
+  }
+  function pauseSlider(){
+      clearInterval(interval);
+
+  }
+  startSlider();
+
+  $slideContainer
+      .on(`mouseenter`, pauseSlider)
+      .on(`mouseleave`, startSlider);
+
+});
